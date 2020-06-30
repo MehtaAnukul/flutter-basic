55818d9d065c90f57c31cce9bc0bcd8de734270d
anukul mehta <mehtaanukul@gmail.com>
2020-06-30 18:05:00 +0530

    calculator

---
+++ flutter_calculator/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercalculator/simple_calculator.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main(){
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculator",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+
+
